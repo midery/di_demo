c4bdcd51693d7cd6f54889f0e5221e3b7b9fb0fb
mvzinchenko <mikhail.zinchenko@traderepublic.com>
2023-08-23 13:05:45 +0200

    Add workflow params

--- ci/profiler/profile_build.sh
+++ ci/profiler/profile_build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-gradle-profiler --benchmark --scenario-file ci/profiler/benchmarks.scenarios --warmups 1 --iteration 1
+gradle-profiler --benchmark \
+  --scenario-file ci/profiler/benchmarks.scenarios \
+  --warmups $WARM_UPS_COUNT --iteration $ITERATIONS_COUNT
