c5d054da83a6f02099f58980ea3df86d27148359
mvzinchenko <mikhail.zinchenko@traderepublic.com>
2023-08-23 12:23:45 +0200

    Fix scenario and profile results

---
+++ ci/profiler/profile_print.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+
+PROFILE_RESULT=$(cat profile-out/benchmark.csv)
+
+SLACK_MESSAGE=$(cat << EOF
+:hammer_and_wrench: Android build measurement complete:
+
+$PROFILE_RESULT
+EOF
+)
+
+echo PROFILE_RESULT
+
+echo "SLACK_MESSAGE<<EOF" >> $GITHUB_ENV
+echo "$SLACK_MESSAGE" >> $GITHUB_ENV
+echo "EOF" >> $GITHUB_ENV
